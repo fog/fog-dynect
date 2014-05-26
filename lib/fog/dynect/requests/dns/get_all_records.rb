fmbiete/fog

Standardise on collection methods
Done with `rubocop --auto-correct --only CollectionMethods`

lib/fog/dynect/requests/dns/get_all_records.rb
@@ -31,9 +31,9 @@ module Fog
           data = [zone[:zone]]
 
           if fqdn = options[:fqdn]
-            data = data | zone[:records].collect { |_type, records| records.select { |record| record[:fqdn] == fqdn } }.flatten.compact
+            data = data | zone[:records].map { |_type, records| records.select { |record| record[:fqdn] == fqdn } }.flatten.compact
           else
-            data = data | zone[:records].collect { |_type, records| records.collect { |record| record[:fqdn] } }.flatten
+            data = data | zone[:records].map { |_type, records| records.map { |record| record[:fqdn] } }.flatten
           end
 
           response.body = {

lib/fog/dynect/requests/dns/get_zone.rb
@@ -31,7 +31,7 @@ module Fog
             }
             info = "get: Your zone, #{zone[:zone]}"
           else
-            data = self.data[:zones].collect { |zone, _data| "/REST/Zone/#{zone}/" }
+            data = self.data[:zones].map { |zone, _data| "/REST/Zone/#{zone}/" }
             info = "get: Your #{data.size} zones"
           end
 

fog/fog-dynect

lib/fog/dynect/requests/dns/get_all_records.rb
@@ -30,9 +30,9 @@ module Fog
           data = [zone[:zone]]
 
           if fqdn = options[:fqdn]
-            data = data | zone[:records].collect { |type, records| records.select { |record| record[:fqdn] == fqdn } }.flatten.compact
+            data = data | zone[:records].map { |type, records| records.select { |record| record[:fqdn] == fqdn } }.flatten.compact
           else
-            data = data | zone[:records].collect { |type, records| records.collect { |record| record[:fqdn] } }.flatten
+            data = data | zone[:records].map { |type, records| records.map { |record| record[:fqdn] } }.flatten
           end
 
           response.body = {

lib/fog/dynect/requests/dns/get_zone.rb
@@ -30,7 +30,7 @@ module Fog
             }
             info = "get: Your zone, #{zone[:zone]}"
           else
-            data = self.data[:zones].collect { |zone, data| "/REST/Zone/#{zone}/" }
+            data = self.data[:zones].map { |zone, data| "/REST/Zone/#{zone}/" }
             info = "get: Your #{data.size} zones"
           end
 